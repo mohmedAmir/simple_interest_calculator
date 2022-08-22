6995f541831f5be3de484d2660d329b2fc620cd4
mohammed ameer <mohat2t@gmail.com>
2022-08-22 11:52:42 +0200

    the Modify

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+
+    document.getElementById("result").innerHTML = (p * r * y ) / 100;
     
 }
         
